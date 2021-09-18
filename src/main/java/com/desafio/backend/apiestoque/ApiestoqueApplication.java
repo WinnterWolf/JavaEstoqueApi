62efefb84b7ba744672acdea567ee7b55b7ea856
WinnterWolf <patrick.winter@outlook.com.br>
2021-09-17 21:09:45 -0300

    First commit

---
+++ src/main/java/com/desafio/backend/apiestoque/ApiestoqueApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.backend.apiestoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiestoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiestoqueApplication.class, args);
+	}
+
+}
